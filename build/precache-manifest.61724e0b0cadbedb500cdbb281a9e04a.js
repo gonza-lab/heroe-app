728aae1228617f7403eed0f2394e7105a935fad3
Gonzalo Flores <gonzaflores358@gmail.com>
2020-09-30 15:01:02 -0300

    .

---
+++ build/precache-manifest.61724e0b0cadbedb500cdbb281a9e04a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9aa4a319c7c968fdab6ae04d9b5e5200",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2d6f019dd27da1364dc8",
+    "url": "/static/css/main.58846194.chunk.css"
+  },
+  {
+    "revision": "6dde89a88ae94116bb20",
+    "url": "/static/js/2.ec143831.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.ec143831.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d6f019dd27da1364dc8",
+    "url": "/static/js/main.0cc1d1d6.chunk.js"
+  },
+  {
+    "revision": "94d7ca25d4df19621824",
+    "url": "/static/js/runtime-main.b7f813af.js"
+  }
+]);
